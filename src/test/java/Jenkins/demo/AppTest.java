004cbbb0588895dc14668194b4decc4732b2cfe8
Ravisha <ravishacr@gmail.com>
2021-07-08 12:09:49 +0530

    first commit

---
+++ src/test/java/Jenkins/demo/AppTest.java
@@ -0,0 +1,26 @@
+package Jenkins.demo;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void add() {
+    	assertFalse(false);
+    }
+}
